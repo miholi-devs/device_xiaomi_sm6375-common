fce7113151d7ccb62cf2a13bf63a229ba2f2f1be
John Galt <johngaltfirstrun@gmail.com>
2023-08-31 10:46:52 +0300

    sm6375-common: Make dex2oat go fast
    Make ota updater go a bit faster.
    
    Change-Id: I3159a0d16d8ce9c6fcb3a138384f2e2f47200a57
    Signed-off-by: JaswalAshish <ashish@m.ms.evolution-x.org>

--- holi.mk
+++ holi.mk
@@ -110,6 +110,17 @@ PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := verify
 PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
 USE_DEX2OAT_DEBUG := false
 
+# Disable SF configstore
+PRODUCT_PACKAGES += \
+    disable_configstore
+
+# Dex to make OTA go faster
+PRODUCT_VENDOR_PROPERTIES += \
+    dalvik.vm.boot-dex2oat-cpu-set=1,2,3,4,5,6,7 \
+    dalvik.vm.boot-dex2oat-threads=7 \
+    dalvik.vm.image-dex2oat-cpu-set=0,1,2,3,4,5,6,7 \
+    dalvik.vm.image-dex2oat-threads=8
+
 # Display
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0.vendor \
